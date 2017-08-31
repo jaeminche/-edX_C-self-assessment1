46ecb0491134a33ced540bdc0d536406e7d5def2
jaeminche <jaeminche@gmail.com>
2017-08-31 18:10:50 +0900

    Create variables

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //teachers and students' info
+            string firstName = "";
+            string lastName = "";
+            DateTime birthdate;
+            string addressLine1 = "";
+            string addressLine2= "";
+            string city= "";
+            string state_province = "";
+            int zip_postal = 0;
+            string country = "";
+
+            //UProgram info
+            string programName = "";
+            string departmentHead = "";
+            string degrees = "";
+
+            //degree info
+            string degreeName = "";
+            bool creditsRequired;
+
+            //course info
+            string courseName = "";
+            short credits = 0;
+            short duration_in_wks = 0;
+            string teacher = "";
+
         }
     }
 }
